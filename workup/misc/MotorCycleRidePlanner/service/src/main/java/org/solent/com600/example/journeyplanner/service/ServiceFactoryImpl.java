a6c0f496f1178999bbecc6b46461fcaba1b3eaf6
Craig Gallen <Craig Gallen>
2019-02-26 09:10:35 +0000

    basic authentication filter works

--- workup/misc/MotorCycleRidePlanner/service/src/main/java/org/solent/com600/example/journeyplanner/service/ServiceFactoryImpl.java
+++ workup/misc/MotorCycleRidePlanner/service/src/main/java/org/solent/com600/example/journeyplanner/service/ServiceFactoryImpl.java
@@ -6,6 +6,7 @@
 package org.solent.com600.example.journeyplanner.service;
 
 import javax.naming.AuthenticationException;
+import javax.persistence.NoResultException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.solent.com600.example.journeyplanner.jpadao.DAOFactory;
@@ -25,6 +26,8 @@ public class ServiceFactoryImpl implements ServiceFactory {
 
     public static final String DEFAULT_ADMIN_USER = "admin";
     public static final String DEFAULT_ADMIN_USER_PASSWORD = "admin";
+    public static final String DEFAULT_ANONYMOUS_USER = "anonymous";
+    public static final String DEFAULT_ANONYMOUS_USER_PASSWORD = "anonymous";
 
     private static ServiceFacadeImpl serviceFacade = null;
 
@@ -33,19 +36,37 @@ public class ServiceFactoryImpl implements ServiceFactory {
         SysUserDAO sysUserDao = DAOFactory.getSysUserDAO();
 
         // add admin user if not present
-        if (sysUserDao.retrieveByUserName("admin") == null) {
-            LOG.warn("admin user not present in database. Created default admin user. You should change password");
-            SysUser adminUser1 = new SysUser();
+        SysUser adminUser1 = null;
+        try {
+            adminUser1 = sysUserDao.retrieveByUserName(DEFAULT_ADMIN_USER);
+        } catch (NoResultException nrex) {
+        }
+        if (adminUser1 == null) {
+            LOG.warn("admin user not present in database. Creating default admin user. You should change password");
+            adminUser1 = new SysUser();
             adminUser1.setUserName(DEFAULT_ADMIN_USER);
             adminUser1.setPassWordHash(PasswordUtils.hashPassword(DEFAULT_ADMIN_USER_PASSWORD));
-            adminUser1.getUserInfo().setFirstname("Default Admin User First Name");
-            adminUser1.getUserInfo().setSurname("Default Admin User Surname");
+            adminUser1.getUserInfo().setFirstname(DEFAULT_ADMIN_USER);
+            adminUser1.getUserInfo().setSurname(DEFAULT_ADMIN_USER);
             adminUser1.setRole(Role.ADMIN);
-  
             sysUserDao.create(adminUser1);
-         
         }
-
+        // add anonymous user if not present
+        SysUser anonUser1 = null;
+        try {
+            anonUser1 = sysUserDao.retrieveByUserName(DEFAULT_ANONYMOUS_USER);
+        } catch (NoResultException nrex) {
+        }
+        if (anonUser1 == null) {
+            LOG.warn("admin user not present in database. Creating default admin user. You should change password");
+            anonUser1 = new SysUser();
+            anonUser1.setUserName(DEFAULT_ANONYMOUS_USER);
+            anonUser1.setPassWordHash(PasswordUtils.hashPassword(DEFAULT_ANONYMOUS_USER_PASSWORD));
+            anonUser1.getUserInfo().setFirstname(DEFAULT_ANONYMOUS_USER);
+            anonUser1.getUserInfo().setSurname(DEFAULT_ANONYMOUS_USER);
+            anonUser1.setRole(Role.ANONYMOUS);
+            sysUserDao.create(anonUser1);
+        }
         serviceFacade.setSysUserDAO(sysUserDao);
     }
 
